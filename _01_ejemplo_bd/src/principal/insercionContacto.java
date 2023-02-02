c852816f60949024a241c0f7366f6783b0ac6252
manana <manana@A13E22>
2023-02-02 13:58:53 +0100

    02

---
+++ _01_ejemplo_bd/src/principal/insercionContacto.java
@@ -0,0 +1,15 @@
+package principal;
+
+public class insercionContacto {
+
+	public static void main(String[] args) {
+
+		//paso 1: establecer la conexion con la BD
+		
+		String cadenaCon="jdbc:mysql://localhost:3306/agenda";
+		String user="root";
+		String pwd="root";
+		
+	}
+
+}
